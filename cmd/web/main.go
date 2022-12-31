b46a5e534fc573ca8cc74962abcfe37202d23ed6
marie-0405 <sirisuamari0405@yahoo.co.jp>
2023-01-01 00:36:39 +0900

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/marie-0405/go_myapp/pkg/config"
+	"github.com/marie-0405/go_myapp/pkg/handlers"
+	"github.com/marie-0405/go_myapp/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	//change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+	app.Session = session
+
+	render.NewTemplates(&app)
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
